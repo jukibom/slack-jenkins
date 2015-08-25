2f95faf4c2330c3b29b11b6bdf7521a9a7db93d8
Jay Faulkner <jayleefaulkner@gmail.com>
2015-08-25 16:16:48 +0100

    Refactor: use job in URL instead of as parameter

--- trigger.php
+++ trigger.php
@@ -8,11 +8,11 @@
     // if 'text' param contains any variables separated by spaces, append to url
     $varArr = explode(" ", $text);
 
-    $params = "";
+    $params = '?token=' . $token;
     foreach ($varArr as $var) {
-        $params .= "&" . $var;
+        $params .= '&' . $var;
     }
 
-    file_get_contents($config['jenkins_url'] . '/buildByToken/buildWithParameters' . $params,0);
+    file_get_contents($config['jenkins_url'] . '/job/' . $job_name . '/buildWithParameters' . $params,0);
 
 ?>
