7da0d0fafc084e1dbd9c769cba95806379c56bf1
Jay Faulkner <jayleefaulkner@gmail.com>
2015-08-25 16:14:16 +0100

    Add: Support for multiple space-delimited variables from slack

--- trigger.php
+++ trigger.php
@@ -1,9 +1,18 @@
 <?php
-$config = require('config.php');
-$branch = $_REQUEST['text'];
-$token = $_REQUEST['jenkins_token'];
-$job_name = $_REQUEST['job_name'];
+    
+    $config = require('config.php');
+    $text = $_REQUEST['text'];
+    $token = $_REQUEST['jenkins_token'];
+    $job_name = $_REQUEST['job_name'];
 
-if (!$branch) $branch = "master";
+    // if 'text' param contains any variables separated by spaces, append to url
+    $varArr = explode(" ", $text);
 
-file_get_contents($config['jenkins_url'].'/buildByToken/buildWithParameters?job='.$job_name.'&token='.$token.'&branch='.$branch,0);
+    $params = "";
+    foreach ($varArr as $var) {
+        $params .= "&" . $var;
+    }
+
+    file_get_contents($config['jenkins_url'] . '/buildByToken/buildWithParameters' . $params,0);
+
+?>
